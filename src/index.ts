d2cfa704b221f4adb9c98100094027a0cd02cb6d
grigoreme <grigore997@gmail.com>
2017-12-21 09:28:47 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,26 @@
-import {Modal,ModalHeader,ModalContent,ModalFooter} from "./Modal";
-import {RouteModal} from "./RouteModal";
-import {NgModule} from "@angular/core";
-import {CommonModule} from '@angular/common';
+import { Modal, ModalHeader, ModalContent, ModalFooter } from "./Modal";
+import { RouteModal } from "./RouteModal";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 export { Modal, ModalContent, ModalFooter, ModalHeader } from "./Modal";
 export { RouteModal } from "./RouteModal";
 
 @NgModule({
-    imports: [ CommonModule ],
-    declarations: [
-        Modal,
-        RouteModal,
-        ModalHeader,
-        ModalContent,
-        ModalFooter,
-    ],
-    exports: [
-        Modal,
-        RouteModal,
-        ModalHeader,
-        ModalContent,
-        ModalFooter,
-    ],
+  imports: [CommonModule],
+  declarations: [
+    Modal,
+    RouteModal,
+    ModalHeader,
+    ModalContent,
+    ModalFooter,
+  ],
+  exports: [
+    Modal,
+    RouteModal,
+    ModalHeader,
+    ModalContent,
+    ModalFooter,
+  ],
 })
-export class ModalModule {
-
-}
+export class ModalModule { }
