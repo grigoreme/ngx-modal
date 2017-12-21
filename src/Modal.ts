ca10e7b61e92a75cf7c40d0852d60dadc534bec8
grigoreme <grigore997@gmail.com>
2017-12-21 09:29:12 +0200

    Update Modal.ts

--- src/Modal.ts
+++ src/Modal.ts
@@ -1,32 +1,26 @@
-import {Component, Input, Output, EventEmitter, ElementRef, ViewChild} from "@angular/core";
+import { Component, Input, Output, EventEmitter, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
-    selector: "modal-header",
-    template: `<ng-content></ng-content>`
+  selector: "modal-header",
+  template: `<ng-content></ng-content>`
 })
-export class ModalHeader {
-
-}
+export class ModalHeader { }
 
 @Component({
-    selector: "modal-content",
-    template: `<ng-content></ng-content>`
+  selector: "modal-content",
+  template: `<ng-content></ng-content>`
 })
-export class ModalContent {
-
-}
+export class ModalContent { }
 
 @Component({
-    selector: "modal-footer",
-    template: `<ng-content></ng-content>`
+  selector: "modal-footer",
+  template: `<ng-content></ng-content>`
 })
-export class ModalFooter {
-
-}
+export class ModalFooter { }
 
 @Component({
-    selector: "modal",
-    template: `
+  selector: "modal",
+  template: `
 <div class="modal" 
      tabindex="-1"
      role="dialog"
@@ -38,8 +32,6 @@ export class ModalFooter {
     <div [class]="'modal-dialog ' + modalClass" (click)="preventClosing($event)">
         <div class="modal-content" tabindex="0" *ngIf="isOpened">
             <div class="modal-header">
-                <button *ngIf="!hideCloseButton" type="button" class="close" data-dismiss="modal" [attr.aria-label]="cancelButtonLabel || 'Close'" (click)="close()"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" *ngIf="title">{{ title }}</h4>
                 <ng-content select="modal-header"></ng-content>
             </div>
             <div class="modal-body">
@@ -47,8 +39,6 @@ export class ModalFooter {
             </div>
             <div class="modal-footer">
                 <ng-content select="modal-footer"></ng-content>
-                <button *ngIf="cancelButtonLabel" type="button" class="btn btn-default" data-dismiss="modal" (click)="close()">{{ cancelButtonLabel }}</button>
-                <button *ngIf="submitButtonLabel" type="button" class="btn btn-primary" (click)="onSubmit.emit(undefined)">{{ submitButtonLabel }}</button>
             </div>
         </div>
     </div>
@@ -56,121 +46,120 @@ export class ModalFooter {
 `
 })
 export class Modal {
+  // -------------------------------------------------------------------------
+  // Inputs
+  // -------------------------------------------------------------------------
 
-    // -------------------------------------------------------------------------
-    // Inputs
-    // -------------------------------------------------------------------------
+  @Input()
+  public modalClass: string;
 
-    @Input()
-    public modalClass: string;
+  @Input()
+  public closeOnEscape: boolean = true;
 
-    @Input()
-    public closeOnEscape: boolean = true;
+  @Input()
+  public closeOnOutsideClick: boolean = true;
 
-    @Input()
-    public closeOnOutsideClick: boolean = true;
+  @Input()
+  public title: string;
 
-    @Input()
-    public title: string;
+  @Input()
+  public hideCloseButton = false;
 
-    @Input()
-    public hideCloseButton = false;
+  @Input()
+  public cancelButtonLabel: string;
 
-    @Input()
-    public cancelButtonLabel: string;
+  @Input()
+  public submitButtonLabel: string;
 
-    @Input()
-    public submitButtonLabel: string;
+  @Input()
+  public backdrop: boolean = true;
 
-    @Input()
-    public backdrop:boolen = true;
+  // -------------------------------------------------------------------------
+  // Outputs
+  // -------------------------------------------------------------------------
 
-    // -------------------------------------------------------------------------
-    // Outputs
-    // -------------------------------------------------------------------------
+  @Output()
+  public onOpen = new EventEmitter(false);
 
-    @Output()
-    public onOpen = new EventEmitter(false);
+  @Output()
+  public onClose = new EventEmitter(false);
 
-    @Output()
-    public onClose = new EventEmitter(false);
+  @Output()
+  public onSubmit = new EventEmitter(false);
 
-    @Output()
-    public onSubmit = new EventEmitter(false);
+  // -------------------------------------------------------------------------
+  // Public properties
+  // -------------------------------------------------------------------------
 
-    // -------------------------------------------------------------------------
-    // Public properties
-    // -------------------------------------------------------------------------
+  public isOpened = false;
 
-    public isOpened = false;
+  // -------------------------------------------------------------------------
+  // Private properties
+  // -------------------------------------------------------------------------
 
-    // -------------------------------------------------------------------------
-    // Private properties
-    // -------------------------------------------------------------------------
+  @ViewChild("modalRoot")
+  public modalRoot: ElementRef;
 
-    @ViewChild("modalRoot")
-    public modalRoot: ElementRef;
+  private backdropElement: HTMLElement;
 
-    private backdropElement: HTMLElement;
+  // -------------------------------------------------------------------------
+  // Constructor
+  // -------------------------------------------------------------------------
 
-    // -------------------------------------------------------------------------
-    // Constructor
-    // -------------------------------------------------------------------------
+  constructor() {
+    this.createBackDrop();
+  }
 
-    constructor() {
-        this.createBackDrop();
+  // -------------------------------------------------------------------------
+  // Lifecycle Methods
+  // -------------------------------------------------------------------------
+
+  ngOnDestroy() {
+    document.body.className = document.body.className.replace(/modal-open\b/, "");
+    if (this.backdropElement && this.backdropElement.parentNode === document.body)
+      document.body.removeChild(this.backdropElement);
+  }
+
+  // -------------------------------------------------------------------------
+  // Public Methods
+  // -------------------------------------------------------------------------
+
+  open(...args: any[]) {
+    if (this.isOpened)
+      return;
+
+    this.isOpened = true;
+    this.onOpen.emit(args);
+    document.body.appendChild(this.backdropElement);
+    window.setTimeout(() => this.modalRoot.nativeElement.focus(), 0);
+    document.body.className += " modal-open";
+  }
+
+  close(...args: any[]) {
+    if (!this.isOpened)
+      return;
+
+    this.isOpened = false;
+    this.onClose.emit(args);
+    document.body.removeChild(this.backdropElement);
+    document.body.className = document.body.className.replace(/modal-open\b/, "");
+  }
+
+  // -------------------------------------------------------------------------
+  // Private Methods
+  // -------------------------------------------------------------------------
+
+  public preventClosing(event: MouseEvent) {
+    event.stopPropagation();
+  }
+
+  private createBackDrop() {
+    this.backdropElement = document.createElement("div");
+    this.backdropElement.classList.add("fade");
+    this.backdropElement.classList.add("in");
+    if (this.backdrop) {
+      this.backdropElement.classList.add("modal-backdrop");
     }
-
-    // -------------------------------------------------------------------------
-    // Lifecycle Methods
-    // -------------------------------------------------------------------------
-
-    ngOnDestroy() {
-        document.body.className = document.body.className.replace(/modal-open\b/, "");
-        if (this.backdropElement && this.backdropElement.parentNode === document.body)
-            document.body.removeChild(this.backdropElement);
-    }
-
-    // -------------------------------------------------------------------------
-    // Public Methods
-    // -------------------------------------------------------------------------
-
-    open(...args: any[]) {
-        if (this.isOpened)
-            return;
-        
-        this.isOpened = true;
-        this.onOpen.emit(args);
-        document.body.appendChild(this.backdropElement);
-        window.setTimeout(() => this.modalRoot.nativeElement.focus(), 0);
-        document.body.className += " modal-open";
-    }
-
-    close(...args: any[]) {
-        if (!this.isOpened)
-            return;
-
-        this.isOpened = false;
-        this.onClose.emit(args);
-        document.body.removeChild(this.backdropElement);
-        document.body.className = document.body.className.replace(/modal-open\b/, "");
-    }
-
-    // -------------------------------------------------------------------------
-    // Private Methods
-    // -------------------------------------------------------------------------
-
-    public preventClosing(event: MouseEvent) {
-        event.stopPropagation();
-    }
-
-    private createBackDrop() {
-        this.backdropElement = document.createElement("div");
-        this.backdropElement.classList.add("fade");
-        this.backdropElement.classList.add("in");
-        if(this.backdrop) {
-            this.backdropElement.classList.add("modal-backdrop");
-        }
-    }
+  }
 
 }
